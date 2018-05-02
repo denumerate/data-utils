e718635dc21665b9db411c71e017dfe0bf205b36
denumerate <pauljricks@gmail.com>
2018-05-02 15:38:20 +0000

    error function now a result of dataset class

--- src/Data/Model.hs
+++ src/Data/Model.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 module Data.Model
   ( Model(..)
-  , ErrorFunction
   , DataSet(..)
   , kfoldCV
   ) where
@@ -26,17 +25,19 @@ class DataSet d => Model m d i o where
     -> d o -- ^ The new values.
   -- |Takes a model, a data set, and uses an error function to produce a
   -- collection of error values.
-  testModel :: ErrorFunction d t o a
+  testModel :: (o -> o -> a) -- ^ The error function.
     -> d i -- ^ The data set being used.
     -> d o -- ^ The associated output values.
     -> m -- ^ The model being tested.
-    -> t a -- ^ The collected error values.
-  testModel ef input output = ef output . predict input
+    -> d a -- ^ The collected error values.
+  testModel ef input output = combineBy ef output . predict input
 
 -- |Basic operations required to work with a data set.
 class DataSet d where
   -- |Splits a data set into two using a function.
-  split :: (a -> (b,c)) -> d a -> (d b,d c)
+  splitBy :: (a -> (b,c)) -> d a -> (d b,d c)
+  -- |Combines two data sets using a function.
+  combineBy :: (a -> b -> c) -> d a -> d b -> d c
   -- |Combines two data sets with the same record type.
   -- Allows for error throwing if the records do not match.
   append :: MonadError e m => d a -> d a -> m (d a)
@@ -46,18 +47,15 @@ class DataSet d where
   -- |Splits a data set into n random partitions.
   partition :: (MonadRandom m) => Int -> d a -> m (Vector (d a))
 
--- |A function that takes two data sets and produces a collection of error scores.
-type ErrorFunction d t a b = d a -> d a -> t b
-
-kfoldCV :: forall m d mr e me t a i o b .
+kfoldCV :: forall m d mr e me a i o b .
   (DataSet d,MonadRandom mr,MonadError e me,Model m d i o) =>
-  ErrorFunction d t o a -> (b -> (i,o)) -> d b -> Int -> mr (me [t a])
+  (o -> o -> a) -> (b -> (i,o)) -> d b -> Int -> mr (me [d a])
 kfoldCV ef sf dset n = partition n dset >>=
   \ds -> return $ mapM (\i -> concatSet (V.toList $
                                          V.ifilter (\i' _ -> i'/=i) ds) >>=
                          \ds' ->
-                           return (let (ia,oa) = split sf ds'
-                                       (ib,ob) = split sf (ds V.! i) in
+                           return (let (ia,oa) = splitBy sf ds'
+                                       (ib,ob) = splitBy sf (ds V.! i) in
                                       testModel ef ib ob
                                       (trainModel ia oa :: m)))
          [0..n]
