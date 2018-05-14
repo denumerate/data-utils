4d000911bcb8dc857f593cb60ba93b46452fbba0
denumerate <pauljricks@gmail.com>
2018-05-14 21:35:37 +0000

    Text added to monadError type

--- src/Data/Model.hs
+++ src/Data/Model.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE TypeFamilies, MultiParamTypeClasses, ScopedTypeVariables #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE AllowAmbiguousTypes, FlexibleContexts #-}
 module Data.Model
   ( Model(..)
   , kfoldCV
   ) where
 
 import Data.List(splitAt)
+import Data.Text(Text)
 import Numeric.LinearAlgebra.Data(Matrix,toRows,fromRows)
 import Numeric.LinearAlgebra(Element)
 import qualified Data.Vector as V
@@ -19,19 +20,19 @@ class Model m i o where
   -- |A set of parameters used to build a model.
   data ModelParams i o
   -- |Trains a model using a provided data set.
-  trainModel :: MonadError e me =>
+  trainModel :: MonadError Text me =>
     ModelParams i o
     -> Matrix i -- ^ The data set used to train the model.
     -> [o] -- ^ The output data.
     -> me (m i o) -- ^ The produced model.
   -- |Uses a data set to predict a new data set of values.
-  predict :: MonadError e me =>
+  predict :: MonadError Text me =>
     Matrix i -- ^ The data set to predict new values with.
     -> m i o -- ^ The model to use to predict.
     -> me [o] -- ^ The new values.
   -- |Takes a model, a data set, and uses an error function to produce a
   -- collection of error values.
-  testModel :: MonadError e me =>
+  testModel :: MonadError Text me =>
     (o -> o -> a) -- ^ The error function.
     -> Matrix i -- ^ The data set being used.
     -> [o] -- ^ The associated output values.
@@ -40,8 +41,8 @@ class Model m i o where
   testModel ef input output = fmap (zipWith ef output) . predict input
 
 -- |Performs k-fold cross-validation on a model system using the supplied data.
-kfoldCV :: forall m mr e me a i o .
-  (MonadRandom mr,MonadError e me,Model m i o,Element i) =>
+kfoldCV :: forall m mr me a i o .
+  (MonadRandom mr,MonadError Text me,Model m i o,Element i) =>
   (o -> o -> a) -> ModelParams i o -> Matrix i -> [o] -> Int
   -> mr (me [[a]])
 kfoldCV ef params ins outs n =
