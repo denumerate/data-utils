63bc557e9332e4a29536b7ae92f0e4061a8960a9
denumerate <pauljricks@gmail.com>
2018-05-15 18:37:46 +0000

    kvfoldM

--- src/Data/Model.hs
+++ src/Data/Model.hs
@@ -1,9 +1,9 @@
-{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, ScopedTypeVariables #-}
-{-# LANGUAGE AllowAmbiguousTypes, FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses, ScopedTypeVariables #-}
 module Data.Model
   ( Model
   , ModelM
   , kfoldCV
+  , kfoldCVM
   ) where
 
 import Data.List(splitAt)
@@ -13,7 +13,11 @@ import qualified Data.Vector as V
 import Control.Monad.Random.Class(MonadRandom)
 import System.Random.Shuffle(shuffleM)
 
+-- |In their simplest form, models are just functions that take one matrix
+-- and return another.
 type Model i o = Matrix i -> Matrix o
+
+-- |Monadic models, allowing failure, etc.
 type ModelM m i o = Matrix i -> m (Matrix o)
 
 -- |Performs k-fold cross-validation on a model system using the supplied data.
@@ -31,8 +35,27 @@ kfoldCV bf ef ins outs n =
                       train' = concat $ reverse train in
                 ef (fromColumns $ map snd test) $
                 bf (fromRows $ map fst train') (fromRows $ map snd train') $
-            fromRows $ map fst test)
+                fromRows $ map fst test)
        [0..n -1]) . splitN n) <$> shuffleM (zip (toRows ins) (toRows outs))
+
+-- |Performs k-fold cross-validation on a model system using the supplied data.
+kfoldCVM :: forall m a i o .
+  (MonadRandom m,Element i,Element o) =>
+  (Matrix i -> Matrix o -> ModelM m i o) ->
+  (Matrix o -> Matrix o -> Matrix a) -> Matrix i -> Matrix o -> Int
+  -> m [Matrix a]
+kfoldCVM bf ef ins outs n =
+  shuffleM (zip (toRows ins) (toRows outs)) >>=
+  ((\ps -> let ps' = V.fromList ps in
+       mapM (\i -> let (test,train) =
+                        V.ifoldl' (\(a,b) i' val -> if i' == i
+                                     then (val,b) else (a,val:b))
+                         ([],[]) ps'
+                       train' = concat $ reverse train in
+                ef (fromColumns $ map snd test) <$>
+                bf (fromRows $ map fst train') (fromRows $ map snd train')
+                (fromRows $ map fst test))
+       [0..n -1]) . splitN n)
 
 -- |Splits a list into n sub-lists.
 splitN :: Int -> [a] -> [[a]]
