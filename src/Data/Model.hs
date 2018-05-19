30208bdfa25d67914f5784337ff47f03ca60f260
denumerate <pauljricks@gmail.com>
2018-05-19 20:54:38 +0000

    monadic build functions

--- src/Data/Model.hs
+++ src/Data/Model.hs
@@ -21,22 +21,23 @@ type Model i o = Matrix i -> Matrix o
 type ModelM m i o = Matrix i -> m (Matrix o)
 
 -- |Performs k-fold cross-validation on a model system using the supplied data.
-kfoldCV :: forall mr a i o .
-  (MonadRandom mr,Element i,Element o) =>
+kfoldCV :: forall m a i o .
+  (MonadRandom m,Element i,Element o) =>
   (Matrix i -> Matrix o -> Model i o) ->
-  (Matrix o -> Matrix o -> Matrix a) -> Matrix i -> Matrix o -> Int
-  -> mr [Matrix a]
+  (Matrix o -> Matrix o -> m (Matrix a)) -> Matrix i -> Matrix o -> Int
+  -> m [Matrix a]
 kfoldCV bf ef ins outs n =
+  shuffleM (zip (toRows ins) (toRows outs)) >>=
   ((\ps -> let ps' = V.fromList ps in
-       map (\i -> let (test,train) =
+       mapM (\i -> let (test,train) =
                         V.ifoldl' (\(a,b) i' val -> if i' == i
                                      then (val,b) else (a,val:b))
                          ([],[]) ps'
-                      train' = concat $ reverse train in
-                ef (fromColumns $ map snd test) $
+                       train' = concat $ reverse train in
+                ef (fromColumns $ map snd test) <$>
                 bf (fromRows $ map fst train') (fromRows $ map snd train') $
                 fromRows $ map fst test)
-       [0..n -1]) . splitN n) <$> shuffleM (zip (toRows ins) (toRows outs))
+       [0..n -1]) . splitN n)
 
 -- |Performs k-fold cross-validation on a model system using the supplied data.
 kfoldCVM :: forall m a i o .
